fec0247565ec3e3a0d9affd385b40cc8143f6978
AliReza Boroujerdian <boroujerdian.alireza@gmail.com>
2024-05-01 19:54:27 +0330

    Add test structure

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,1 +1,22 @@
-use email-newsletter::main;
+async fn spawn_app() -> Result<(), std::io::Error> {
+    todo!()
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    spawn_app().await.expect("Failed to spawn our app.");
+
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get("http://127.0.0.1:8080/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
